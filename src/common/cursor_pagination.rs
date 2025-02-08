d849187f81d25f8c503c0018fe72205843a1bc18
Dylan Valentin <dylan.valentin@ik.me>
2025-02-08 21:59:10 +0100

    refactor(CursorPagination): default function name

--- src/common/cursor_pagination.rs
+++ src/common/cursor_pagination.rs
@@ -1,14 +1,14 @@
 use serde::{Deserialize, Serialize};
 
-const DEFAULT_PAGINATION: usize = 1;
+const DEFAULT_PAGINATION_LIMIT: u32 = 1;
 
-fn get_default_pagination() -> usize {
-    DEFAULT_PAGINATION
+fn get_default_pagination_limit() -> u32 {
+    DEFAULT_PAGINATION_LIMIT
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
 pub struct CursorPagination {
-    #[serde(default = "get_default_pagination")]
-    pub limit: usize,
+    #[serde(default = "get_default_pagination_limit")]
+    pub limit: u32,
     pub cursor_after: Option<String>,
 }
