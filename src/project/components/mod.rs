2cf7edb10190109e98f2ebc4666527b0fc6bde5a
Dylan Valentin <dylan.valentin@ik.me>
2025-02-17 18:48:28 +0100

    feat(project): project card component

--- src/project/components/mod.rs
+++ src/project/components/mod.rs
@@ -1,3 +1,4 @@
+pub mod project_card;
 pub mod project_card_skeleton;
 pub mod project_tag;
 pub mod project_thumbnail;
---
+++ src/project/components/project_card.rs
@@ -0,0 +1,27 @@
+use leptos::prelude::*;
+
+use crate::project::{
+    components::{
+        project_tag::ProjectTag, project_thumbnail::ProjectThumbnail,
+    },
+    data::project_context::ProjectContext,
+};
+
+#[component]
+pub fn ProjectCard(project_context: ProjectContext) -> impl IntoView {
+    view! {
+        <div class="tw-project-card">
+            <ProjectThumbnail project_context=project_context.clone() />
+
+            <span class="tw-card-title">
+                { project_context.title }
+            </span>
+
+            <div class="tw-card-tag-container">
+                <For each=move || project_context.tags.0.clone() key=|tag| tag.clone() let:tag>
+                    <ProjectTag text=tag />
+                </For>
+            </div>
+        </div>
+    }
+}
