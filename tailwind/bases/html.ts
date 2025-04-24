8e60701b8ba117fce48a5d3f4840a9b3a848422f
Dylan Valentin <dylan.valentin@ik.me>
2025-04-24 10:59:40 +0200

    fix(Tailwind): disable scroll-smooth when reduced motion

--- tailwind/bases/html.ts
+++ tailwind/bases/html.ts
@@ -5,6 +5,11 @@ export const addHtmlBase: ComponentBuilderFunction = ({ addBase }, { classPrefix
     html: {
       // Others
       [`@apply ${classPrefix}scroll-smooth`]: {},
+
+      //// https://css-tricks.com/fixing-smooth-scrolling-with-find-on-page/
+      '@media screen and (prefers-reduced-motion: reduce)': {
+        [`@apply ${classPrefix}scroll-auto`]: {},
+      },
     },
   });
 };
