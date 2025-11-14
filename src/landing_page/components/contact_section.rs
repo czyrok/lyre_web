bfa749c5396e059f016a979065b569c44ce7db72
Dylan Valentin <dylan.valentin@ik.me>
2025-11-14 19:26:36 +0100

    feat(assets): my photo

--- src/landing_page/components/contact_section.rs
+++ src/landing_page/components/contact_section.rs
@@ -1,4 +1,5 @@
 use leptos::prelude::*;
+use leptos_meta::*;
 
 use crate::{
     core::data::icon_set::IconSet,
@@ -17,15 +18,13 @@ use crate::{
 #[component]
 pub fn ContactSection() -> impl IntoView {
     view! {
-        //// Causes issue in static mode when I go to '/projects' from '/'
-        //// The head of the document will be impacted, and, Tailwind output will not be loaded
-        // <Link rel="preload" href="/images/ma_photo.png" as_="image" type_="image/png" />
+        <Link rel="preload" href="/images/ma_photo.webp" as_="image" type_="image/webp" />
 
         <div class="tw-landing-page-section-container tw-landing-page-contact-section">
             <div id="contact" class="tw-anchor"></div>
 
             <div class="tw-section-photo">
-                <img src="/images/ma_photo.png" alt="Photo of the owner of the website" />
+                <img src="/images/ma_photo.webp" alt="Photo of the owner of the website" />
             </div>
 
             <div class="tw-section-right">
